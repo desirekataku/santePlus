1b9befd3164e0a4b7b8acdaee40c76f3793e1d87
desirekataku <desirekataku552@gmail.com>
2025-07-24 17:20:43 +0100

    last moov1

--- app.py
+++ app.py
@@ -85,3 +85,11 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+
+
+python
+Copier
+Modifier
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
